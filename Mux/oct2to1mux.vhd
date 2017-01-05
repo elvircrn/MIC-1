32d4257504d7737f284fd1de8798be0833502e42
Amar Buric <amaron_96@live.com>
2017-01-05 16:54:48 +0100

    Added multiplexer project

---
+++ Mux/oct2to1mux.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:52:12 01/05/2017 
+-- Design Name: 
+-- Module Name:    oct2to1mux - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity oct2to1mux is
+port(
+        x0, x1 : in std_logic_vector(7 downto 0);
+        y0 : out std_logic_vector(7 downto 0);
+        c : in std_logic
+);
+end oct2to1mux;
+
+architecture Behavioral of oct2to1mux is
+
+begin
+
+y0 <= x1 when c = '1' else
+                x0;
+					 
+end Behavioral;
+
